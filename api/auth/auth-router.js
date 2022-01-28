c6878a0322994a4fd98729640fbdd3b098dcef0d
Justin Peisker <justinpeisker@gmail.com>
2022-01-28 10:42:34 -0800

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -5,14 +5,9 @@ const { checkUsernameExists, unAndPassRequired  } = require('./auth-middleware')
 const { JWT_SECRET } = require("../../secrets");
 const jwt = require('jsonwebtoken')
 
-router.post('/register', checkUsernameExists, unAndPassRequired, (req, res, next) => {
+router.post('/register', unAndPassRequired,checkUsernameExists, (req, res, next) => {
   let user = req.body
   const hash = bcrypt.hashSync(user.password, 8)
-  
-
-  // if(!user.username || !user.password) {
-  //     next({status: 401, message: "username and password required"})
-  // }
   user.password = hash
   User.add(user)
     .then(newUser => {
@@ -87,30 +82,6 @@ router.post('/register', checkUsernameExists, unAndPassRequired, (req, res, next
 });
 
 router.post('/login', unAndPassRequired, (req, res, next) => {
-  // res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
 
 let { username, password } = req.body
 
